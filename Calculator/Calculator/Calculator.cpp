20708874a75dd69876b5bbd32141fe0a50ecdbd0
TheSherlockHomie <coolman.kushagra@gmail.com>
2016-06-09 16:09:35 +0530

    Initial version. Added +, - and basic error handling.

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -2,10 +2,40 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <limits>
 
-
+void keepConsoleOpen();
 int main()
 {
-    return 0;
+	int result, rightValue, leftValue = 0;
+	char op;
+	std::cout << "Enter an expression  ('+' and '-' supported)" << std::endl;
+	std::cin >> leftValue >> op >> rightValue;             //Read input like 1 + #
+	std::cin.get();
+
+	{                                                      //Calculation
+		if (op == '+')
+			result = leftValue + rightValue;
+		else if (op == '-')
+			result = leftValue - rightValue;
+		else
+		{
+			std::cerr << "Invalid Operator!" << std::endl;
+			keepConsoleOpen();
+			return 1;
+		}
+	}
+	
+	std::cout << " = " << result << std::endl;             //Output result
+
+	keepConsoleOpen();
+	return 0;
 }
 
+void keepConsoleOpen()
+{                                                      //To keep console window open until 'Enter' key is pressed
+	std::cout << "Press enter to exit..." << std::endl;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
